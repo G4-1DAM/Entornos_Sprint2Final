2f0f718439bf9778ba3aa301cd2175bfdfdd505b
Daniel del Toro Boigues <93922406+Danieldtb@users.noreply.github.com>
2021-11-25 11:23:16 +0100

    Add files via upload

---
+++ Entornos_Grupo4/src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void HelloWorld() {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World");
+        System.out.println("Hi world");
+        System.out.println("Hello World de nuevo");
+	}
+}
